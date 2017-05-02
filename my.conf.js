528b64d0e808ece30401cecdc7f484737e15ecef
Cefied <cefied@gmail.com>
2017-05-02 18:18:15 +0200

    test added

--- my.conf.js
+++ my.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'static/js/*.js, test/js/*.js'
+      'static/js/*.js, tests/js/*.js'
     ],
 
 
