4ad3149e98df326b8642a5b12cc9bfcde933ce40
László Balázs <crynion@gmail.com>
2017-05-04 17:39:30 +0200

    Add chromedriver. Change jasmine config. Change travis config: put jasmine in script.

---
+++ spec/ui/uiSpec.js
@@ -0,0 +1,46 @@
+const chromedriver = require('chromedriver');
+
+const webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+	
+const MAX_BROWSER_START_TIME_IN_MILLIS = 30000; //30 sec
+	
+var driver;
+
+//jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
+
+describe("Chrome driver", function() {
+	var originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+	
+	beforeAll(function() {
+		jasmine.DEFAULT_TIMEOUT_INTERVAL = MAX_BROWSER_START_TIME_IN_MILLIS; 
+	});
+	
+	afterAll(function() {
+		jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+	});
+	
+	beforeEach(function(done) {
+		let builder = new webdriver.Builder();
+		
+		//build browser
+		builder.forBrowser(webdriver.Browser.CHROME)
+			.build()
+			.then(d => {
+				driver = d;
+				done();
+			});
+	});
+	
+	afterEach(function(done) {
+        driver.quit().then(done);
+    });
+	
+	it("should navigate to google ", function() {
+		driver.get('http://google.com');
+		driver.getTitle().then(function(title) {
+			expect(title).toBe('Google');
+		});
+	});
+});
