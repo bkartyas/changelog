aa9111afa9b6ca4a5076a9517581848048d189d0
László Balázs <crynion@gmail.com>
2017-05-04 23:09:20 +0200

    Add xvfb to travis config instead, as suggested here: https://docs.travis-ci.com/user/gui-and-headless-browsers/#Using-xvfb-to-Run-Tests-That-Require-a-GUI

--- spec/ui/uiSpec.js
+++ spec/ui/uiSpec.js
@@ -1,24 +1,20 @@
 const 	chromedriver = require('chromedriver'),
-		Xvfb = require('xvfb'),
 		webdriver = require('selenium-webdriver'),
 		By = webdriver.By,
 		until = webdriver.until,
 		MAX_BROWSER_START_TIME_IN_MILLIS = 100000; //100 sec
 
-var xvfb = new Xvfb(), 
-	driver;
+var driver;
 
 describe("Chrome driver", function() {
 	var originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
 	
 	beforeAll(function() {
 		jasmine.DEFAULT_TIMEOUT_INTERVAL = MAX_BROWSER_START_TIME_IN_MILLIS;
-		xvfb.startSync();
 	});
 	
 	afterAll(function() {
 		jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
-		xvfb.stopSync();
 	});
 	
 	beforeEach(function(done) {
